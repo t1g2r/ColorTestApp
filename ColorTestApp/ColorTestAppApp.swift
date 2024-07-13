1eb91d12cd7caa0b7ed65afc4fd10e036aae724a
my account <myaccount@usgs-MacBook-Air.local>
2024-07-14 03:27:36 +0900

    Create Xcode Project

---
+++ ColorTestApp/ColorTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTestAppApp.swift
+//  ColorTestApp
+//
+//  Created by my account on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
